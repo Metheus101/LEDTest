f0cf98ebf4bbf1445a77368daf0c6bd258de89e1
Metheus101 <gutekunstmarkus@gmx.net>
2016-05-10 12:28:49 +0200

    Breath effect RGB

--- V1_1-Fach_Test/V1_1-Fach_Test.ino
+++ V1_1-Fach_Test/V1_1-Fach_Test.ino
@@ -1,9 +1,91 @@
-void setup() {
-  // put your setup code here, to run once:
+//---------------------Defines------------------------
 
+#define Pin_R 6
+#define Pin_G 11
+#define Pin_B 3
+
+#define PWM_R 30
+#define PWM_G 100
+#define PWM_B 100
+int PWM = 0;
+
+void setup() {
+pinMode (Pin_G, OUTPUT);
+pinMode (Pin_R, OUTPUT);
+pinMode (3, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+PWM=0;
+analogWrite(Pin_R,0);
+analogWrite(Pin_G,0);
+analogWrite(Pin_B,0);
+for(int a=0; a<255; a++)
+{
+analogWrite(Pin_R,PWM);
 
+PWM++;
+delay (10);
+}
+for(int a=0; a<255; a++)
+{
+analogWrite(Pin_R,PWM);
+
+PWM--;
+delay (10);
+}
+analogWrite(Pin_R,0);
+analogWrite(Pin_G,0);
+analogWrite(Pin_B,0);
+for(int a=0; a<255; a++)
+{
+
+analogWrite(Pin_G,PWM);
+
+PWM++;
+delay (10);
+}
+for(int a=0; a<255; a++)
+{
+
+analogWrite(Pin_G,PWM);
+
+PWM--;
+delay (10);
+}
+analogWrite(Pin_R,0);
+analogWrite(Pin_G,0);
+analogWrite(Pin_B,0);
+for(int a=0; a<255; a++)
+{
+analogWrite(Pin_B,PWM);
+PWM++;
+delay (10);
+}
+for(int a=0; a<255; a++)
+{
+
+analogWrite(Pin_B,PWM);
+PWM--;
+delay (10);
+}
+analogWrite(Pin_R,0);
+analogWrite(Pin_G,0);
+analogWrite(Pin_B,0);
+for(int a=0; a<255; a++)
+{
+analogWrite(Pin_R,PWM);
+analogWrite(Pin_G,PWM);
+analogWrite(Pin_B,PWM);
+PWM++;
+delay (10);
+}
+for(int a=0; a<255; a++)
+{
+analogWrite(Pin_R,PWM);
+analogWrite(Pin_G,PWM);
+analogWrite(Pin_B,PWM);
+PWM--;
+delay (10);
+}
 }
